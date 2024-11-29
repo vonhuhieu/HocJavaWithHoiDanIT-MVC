f4165926e8d179f851a6c22b18959bafc7a4ed47
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-29 09:40:36 +0700

    Kết nối db thành công, thiết lập spring security thành công

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -2,11 +2,15 @@ package vn.hoidanit.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+// @SpringBootApplication
+// include <> exclude
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class LaptopshopApplication {
 
 	public static void main(String[] args) {
+		// container
 		SpringApplication.run(LaptopshopApplication.class, args);
 	}
 
---
+++ src/main/java/vn/hoidanit/laptopshop/service/MyService.java
@@ -0,0 +1,8 @@
+package vn.hoidanit.laptopshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+}
