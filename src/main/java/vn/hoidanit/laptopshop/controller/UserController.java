1758e02987e99b709c4d9db98c982c486a37b7b6
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-29 10:15:41 +0700

    Render ra view bằng cách tạo file html trong thư mục static trong thư mục resources

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -7,27 +7,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-// @Controller
-// public class UserController {
-
-//     @RequestMapping("/")
-//     public String getHomePage() {
-//         return "hello from controller";
-//     }
-// }
-
-@RestController
+@Controller
 public class UserController {
-
-    // DI: dependency injection
+        // DI: dependency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 }
+
+// @RestController
+// public class UserController {
+
+//     // DI: dependency injection
+//     private UserService userService;
+
+//     public UserController(UserService userService) {
+//         this.userService = userService;
+//     }
+
+//     @GetMapping("/")
+//     public String getHomePage() {
+//         return this.userService.handleHello();
+//     }
+// }
