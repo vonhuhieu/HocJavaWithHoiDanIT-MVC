ff54a931e8505e18bcbf3d538cfb443e6f720d4f
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-29 10:51:35 +0700

    Cài jsp, cấu hình view engine trong file config

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,31 +9,32 @@ import vn.hoidanit.laptopshop.service.UserService;
 
 @Controller
 public class UserController {
-        // DI: dependency injection
+    // DI: dependency injection
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @RequestMapping("/")
     public String getHomePage() {
         String test = this.userService.handleHello();
-        return "eric.html";
+        return "hello";
     }
 }
 
 // @RestController
 // public class UserController {
 
-//     // DI: dependency injection
-//     private UserService userService;
+// // DI: dependency injection
+// private UserService userService;
 
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
 
-//     @GetMapping("/")
-//     public String getHomePage() {
-//         return this.userService.handleHello();
-//     }
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
 // }
