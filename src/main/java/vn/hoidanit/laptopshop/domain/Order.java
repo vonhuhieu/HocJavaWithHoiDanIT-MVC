e4a8c0e05f597dc8a869c777a86c3af0a5596698
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-01 05:27:22 +0700

    Biến mối quan hệ N-N giữa Order và Product thành 2 mối quan hệ N-1 bằng cách tạo thêm Order_Product, thiết lập 1 order có thể có nhiều order_detail nhưng mỗi order_detail chỉ thuộc về 1 order và 1 product có thể có nhiều product_detail nhưng mỗi product_detail chỉ thuộc về 1 product,đã viết ra để hiểu

--- src/main/java/vn/hoidanit/laptopshop/domain/Order.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Order.java
@@ -1,11 +1,14 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,7 +19,7 @@ public class Order {
     private long id;
 
     private double totalPrice;
-    
+
     // userID
     // order many -> to one -> user
     @ManyToOne
