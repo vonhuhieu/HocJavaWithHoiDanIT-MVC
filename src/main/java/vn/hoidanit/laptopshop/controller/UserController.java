17550d66d8ab473e956a2b7c1a62b8f45c58557c
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-30 08:32:11 +0700

    Định nghĩa repository querry

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,6 +23,8 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
+        List<User> arrUsers = this.userService.getAllUsersByEmail("1@gmai.com");
+        System.out.println(arrUsers);
         String test = this.userService.handleHello();
         model.addAttribute("eric", "test");
         model.addAttribute("hoidanit", "from controller with model");
