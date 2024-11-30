0fd1a50b9a18be2b4ad295449b3efd17109fed5c
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-11-30 08:04:38 +0700

    Lưu data vào db bằng Repository

---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package vn.hoidanit.laptopshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import vn.hoidanit.laptopshop.domain.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+    User save(User hoidanit);
+}
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -2,9 +2,24 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
+
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello() {
         return "hello from service";
+    }
+
+    public User handleSaveUser(User user){
+        User eric = this.userRepository.save(user);
+        System.out.println(eric);
+        return eric; 
     }
 }
