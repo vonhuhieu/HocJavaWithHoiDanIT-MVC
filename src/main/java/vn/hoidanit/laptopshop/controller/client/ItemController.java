34f0ddf5c6d9edf6401d7ae64d06aaf71ea854ec
Hieu Vo <hieu.vo@sapotacorp.vn>
2024-12-02 17:05:38 +0700

    css dữ liệu số, làm trang detail product

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -1,13 +1,26 @@
 package vn.hoidanit.laptopshop.controller.client;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 @Controller
 public class ItemController {
+    private final ProductService productService;
+
+    public ItemController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
-    public String getProductPage(@PathVariable long id) {
+    public String getProductPage(Model model,
+            @PathVariable long id) {
+        Product product = this.productService.getProductByID(id);
+        model.addAttribute("product", product);
         return "/client/product/detail";
     }
 }
